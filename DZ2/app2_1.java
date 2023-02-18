6390c8de92a72f1386859bb33c3159936b1c0869
Vladimir <vapsan@gmail.com>
2023-02-18 16:52:22 +0300

    Решение 2.1 с помощью функций

--- DZ2/app2_1.java
+++ DZ2/app2_1.java
@@ -1,35 +1,45 @@
 package JAVA_work.DZ2;
 
 public class app2_1 {
-   
-
+   private static int count = 0; //по умолчанию запрос пустой (все null)
+   private static String request = "SELECT* FROM students WHERE";
+   private static String str = "{\"name\":\"Павел\", \"country\":\"Россия\", \"city\":\"null\", \"age\":\"null\"}";
    public static void main(String[] args) {
-      String request = "SELECT* FROM students WHERE";
-      String str = " {\"name\":\"null\", \"country\":\"Russia\", \"city\":\"null\", \"age\":\"11\"}";
-      // String str = " {\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-      System.out.println(str);
+         
+      System.out.println("Строка аргументов запроса:\n" + str);
+      String str_clear = clearStr(str);
+      System.out.println("SQL запрос: \n" + print(makeRequest(str_clear)));
+   }
+
+   public static String clearStr(String str) {
       StringBuilder builder = new StringBuilder(str);
       String str_clear = builder.toString().replace("{", "").replaceAll("\"", "").replace("}", "").replaceAll(" ", "");
-      // System.out.println(str_clear);
-      int count = 0;
-      String[] group = str_clear.split(",");
+      return str_clear;
+   }
+   
+   public static String makeRequest(String str) {
+      String[] group = str.split(",");
       for (String string : group) {
-        String[] itemInfo = string.split(":");
+         String[] itemInfo = string.split(":");
          if (!itemInfo[1].equals("null")) {
-            if (count != 0) {
+            if (count != 0) {                          //Если не нулевых параметров >1, то добавляем AND
                request += " AND";
             }
             count++;
-            request += " "+itemInfo[0] + " = '" + itemInfo[1]+"'";
+            request += " " + itemInfo[0] + " = '" + itemInfo[1] + "'";
          }
       }
       request += ";";
-     if (count==0) {
-        System.out.println("Нет данных для запроса");
-     } else {
-        System.out.println(request);
-     }
-      
+      return request;
    }
-
+  
+   public static String print(String request) {
+      String output = "";
+      if (count == 0) {
+         output = "Нет данных для запроса";
+      } else {
+         output = request;
+      }
+      return output;
+   }
 }
